1fef50a35b14f5c78884297ff5ccd286c75c53ac
Md Rajib Bhuiyan <mdrajibbhuiyan71@gmail.com>
2021-10-28 15:48:17 +0600

    function in javascript

---
+++ Function/function.js
@@ -0,0 +1,23 @@
+//addition
+function addition(a, b){
+    var result = a + b;
+    document.write("Add = " + result + "<br>");
+}
+
+//subtract
+function subtract(a , b){
+    var result = a - b;
+    document.write("Sub = " + result + "<br>");
+ 
+}
+
+function multiplication(a , b){
+    var result = a * b;
+    document.write("Mul = " + result + "<br>");
+}
+
+//calling addition function
+addition(5, 5);
+subtract(8, 5);
+multiplication(8 , 8);
+
